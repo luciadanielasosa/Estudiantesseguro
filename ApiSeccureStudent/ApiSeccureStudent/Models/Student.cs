40864b74ca994126028c938f2191b93f4e6597a6
luciadanielasosa <a2019118793@estudiantes.upsa.edu.bo>
2020-08-29 10:35:19 -0400

    Con ControllerStudents mas autorizacion

---
+++ ApiSeccureStudent/ApiSeccureStudent/Models/Student.cs
@@ -0,0 +1,33 @@
+﻿
+
+
+namespace ApiSeccureStudent.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    public class Student
+    {
+        [Key]
+        [Required]
+        [Range(1, 9999)]
+
+
+        public int StudentID { get; set; }
+        [StringLength(30)]
+        [Required(ErrorMessage = "You must enter the field {0}")]
+
+
+        public string LastName { get; set; }
+        [StringLength(30, ErrorMessage = "The field{0} must contain betwen {1}characters", MinimumLength = 2)]
+        [Required(ErrorMessage = "You must enter the field {0}")]
+
+
+        public string FirsName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+
+
+        public DateTime EnrollmentDate { get; set; }
+
+    }    
+}
